b82b73353737e6622115c671e22860ae1637e05f
k-mora <kevin.mora.2016@gmail.com>
2020-10-02 12:06:04 -0500

    added testing suite

---
+++ XUnitRPS_GameMvc/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitRPS_GameMvc
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
